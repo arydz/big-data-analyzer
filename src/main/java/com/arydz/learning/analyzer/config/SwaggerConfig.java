e95d3247b0d6a9943e68fc198ba453fe0083c819
arydz <driver89.adrian@gmail.com>
2018-04-22 18:52:47 +0200

    Removed commented block of code

--- src/main/java/com/arydz/learning/analyzer/config/SwaggerConfig.java
+++ src/main/java/com/arydz/learning/analyzer/config/SwaggerConfig.java
@@ -32,34 +32,4 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
-/*
-    @Bean
-    public WebMvcRegistrations webMvcRegistrationsHandlerMapping() {
-        return new WebMvcRegistrations() {
-            @Override
-            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
-                return new RequestMappingHandlerMapping() {
-                    private final static String API_BASE_PATH = "/analyzer/api";
-
-                    @Override
-                    protected void registerHandlerMethod(Object handler, Method method, RequestMappingInfo mapping) {
-                        Class<?> beanType = method.getDeclaringClass();
-                        if (AnnotationUtils.findAnnotation(beanType, RestController.class) != null) {
-                            PatternsRequestCondition apiPattern = new PatternsRequestCondition(API_BASE_PATH)
-                                    .combine(mapping.getPatternsCondition());
-
-                            mapping = new RequestMappingInfo(mapping.getName(), apiPattern,
-                                    mapping.getMethodsCondition(), mapping.getParamsCondition(),
-                                    mapping.getHeadersCondition(), mapping.getConsumesCondition(),
-                                    mapping.getProducesCondition(), mapping.getCustomCondition());
-                        }
-
-                        super.registerHandlerMethod(handler, method, mapping);
-                    }
-                };
-            }
-        };
-    }
-*/
-
 }
